49891aacc47e9712046c5768335070f0c11eb14a
MansurovF <112488802+MansurovF@users.noreply.github.com>
2022-12-06 20:15:03 +0400

    Add files via upload

---
+++ Classtask/script.js
@@ -0,0 +1,41 @@
+//task1
+// let arr=[10,15,20,25]
+// function arr(params) {
+//     for (let i = 0; i < arr.length; i++) {
+//         if (i%arr==arr.value) {
+//             console.log(arr.value.length);
+//         }       
+// }
+// }
+//task2
+// let arr=[1,2,3,4,5,6,7,8,9,10];
+// function Myfunction(number) {
+//     let first=[];
+//     let second=[]; 
+//     for (let i = 0; i < arr.length; index++) {
+//         if (arr[i]%2==0) {
+//             first.push=(arr[i])
+//         }
+//         else{
+//             second.push=(arr[i])
+//         }  
+//     }
+//     console.log(first);
+//     console.log(second);
+// }
+
+
+
+
+
+//task3
+// let String=('utu')
+// for (let i = 0; i < String.length; index++) {
+//     if (String[i]!=String(lenght-1-i)) {
+//         console.log(String);
+//     }
+//     else{
+//         console.log(String);
+//     }
+    
+// }
